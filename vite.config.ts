aebe005a48213f0564da43b52d276e34e620fcd2
Minoru Inada <jyagamino@gmail.com>
2025-04-12 00:57:07 +0900

    ワークフローの修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repositoryName = 'map-vs-for-performance'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? `/${repositoryName}/` : '/',
 })
